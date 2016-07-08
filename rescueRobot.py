2bf7c8815abbb071fa30e10687385e288663bb5d
Kapil Kanwar <lipak314@gmail.com>
2016-07-08 00:47:42 -0400

    Add ultrasonic wall hugging and victim tracking (untested)

--- rescueRobot.py
+++ rescueRobot.py
@@ -4,6 +4,7 @@ PI = 3.141592653589793238462643383279
 
 LEFT_MOTOR = M2
 RIGHT_MOTOR = M1
+ULTRASONIC_MOTOR = M3
 LINE_TRACKER = S1
 VICTIM_TRACKER = S2
 ULTRASONIC = S3
@@ -11,6 +12,7 @@ ULTRASONIC = S3
 # Inches
 WHEEL_RADIUS = 1
 ROBOT_RADIUS = 2.8
+ROBOT_LENGTH = 6
 
 ENCODER_KP = 1
 ENCODER_KI = 0
@@ -20,21 +22,51 @@ LINE_TRACKER_KP = 0
 LINE_TRACKER_KI = 0
 LINE_TRACKER_KD = 0
 
+ULTRASONIC_KP = 0
+
 LINE_TRACKER_RED = -1
 LINE_TRACKER_WHITE = -1
 LINE_TRACKER_BLACK = -1
 
+WALL_HUGGING_DISTANCE = 5
+
+# Status variables
+lineTrackingComplete = false
+numVictimsFound = 0
+
+# Track the victims
+def trackVictims():
+	count = 0
+	while (not lineTrackingComplete):
+		curColor = HC.get_colornum()
+		# Dark blue
+		if (curColor == 2 or curColor == 3):
+			PSM.led(1, 255, 0, 0)
+            sleep(3)
+            PSM.led(1, 0, 0, 0)
+            sleep(2)
+        else:
+        	sleep(0.1)
+    numVictimsFound = count
+
 # Follow a line
-def followLine(speed, lineColor, backgroundColor, stopCondition) {
+def followLine(speed, lineColor, backgroundColor, stopCondition, *stopConditionArguments):
 	lightPID = PID((lineColor + backgroundColor) / 2, LINE_TRACKER_KP, LINE_TRACKER_KI, LINE_TRACKER_KD)
-	while (not stopCondition()):
+	while (not stopCondition(*stopConditionArguments)):
 		turningAmount = lightPID.update(LINE_TRACKER.colorSensorNXT())
 		LEFT_MOTOR.setSpeed(-speed + turningAmount)
 		RIGHT_MOTOR.setSpeed(-speed - turningAmount)
 
 	LEFT_MOTOR.brake()
 	RIGHT_MOTOR.brake()
-}
+	lineTrackingComplete = True
+
+# Set the position of the ultrasonic sensor
+def setUltrasonicPosition(degs):
+	ultrasonicPID = PID(-degs, ULTRASONIC_KP, 0, 0)
+	while (ULTRASONIC_MOTOR.pos() != -degs):
+		ultrasonicPID.setSpeed(ultrasonicPID.update(ULTRASONIC_MOTOR.pos()))
+	ULTRASONIC_MOTOR.brake()
 
 # Turn the robot by a certain number of degrees
 def turnRobot(deg, speed, clockwise):
@@ -101,11 +133,48 @@ def isAboveLine(color, threshold):
 	else:
 		return False
 
-def main():
-	# Line following and victim tracking
+def readUltrasonicValueAt(degs):
+	setUltrasonicPosition(degs)
+	return ULTRASONIC.distanceUSEV3in()
+
+def hugRightWall(speed):
+	# If you can turn right, turn right
+	# Else if there is a wall on the right and a wall in front turn left
+	# Else if there is a wall on the right and nothing in front go forward
 	
-	# Traversing the house
-	moveRobot(5, 30, False)
+	# Estimated distance left in front of the robot to the wall
+	distanceInFront = readUltrasonicValueAt(0)
+
+	# False means left turn, True means right turn
+	previousTurn = False
+	while (True):		
+		if (readUltrasonicValueAt(90) > WALL_HUGGING_DISTANCE + 1):
+			if (previousTurn == False):
+				moveRobot(ROBOT_LENGTH, speed, True)
+
+			turnRobot(90, speed, True)
+			moveRobot(max(0, min(ROBOT_LENGTH, readUltrasonicValueAt(0) - WALL_HUGGING_DISTANCE)), speed, True)
+			distanceInFront = readUltrasonicValueAt(0)
+			previousTurn = True
+		else if (distanceInFront < WALL_HUGGING_DISTANCE + 1 and readUltrasonicValueAt(0) < WALL_HUGGING_DISTANCE):
+			turnRobot(90, speed, False)
+			distanceInFront = readUltrasonicValueAt(0)
+			previousTurn = False
+		else:
+			moveRobot(1, speed, True)
+			distanceInFront -= 1
+
+def main():
+	# Init
+	#VICTIM_TRACKER.activateCustomSensorI2C()
+	
+	# Line following and victim tracking
+	#victimThread = Thread(target = trackVictims)
+	#victimThread.start()
+	#followLine(30, LINE_TRACKER_WHITE, LINE_TRACKER_BLACK, isAboveLine, LINE_TRACKER_RED, 30)
+
+	# Traversing the house by following the right wall 	
+	moveRobot(10, 40, True)	
 
 if __name__ == "__main__":
 	main()
